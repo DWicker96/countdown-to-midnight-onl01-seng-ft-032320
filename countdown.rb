a6db3aa6c4f47dd9595c0e86ac8aab9c2d569f86
Dakota Wicker <dwicker1996@gmail.com>
2020-03-25 16:36:58 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -2,8 +2,8 @@ def countdown(num)
   while num > 0
     puts "#{num} SECOND(S)!"
     num -= 1
-    return "HAPPY NEW YEAR!"
-  end
+      end
+  return "HAPPY NEW YEAR!"
 end
 
 def countdown_with_sleep(num)
